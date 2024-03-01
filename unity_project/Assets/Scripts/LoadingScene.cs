c992b04d9629226bb19fea28ed334c5b106ad141
Thisari Piyumika Amarasekara <tpamarasekara@gmail.com>
2024-03-01 20:55:50 +0530

    Updated LoadingScene.cs

--- unity_project/Assets/Scripts/LoadingScene.cs
+++ unity_project/Assets/Scripts/LoadingScene.cs
@@ -1,4 +1,8 @@
 using System.Collections;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -13,28 +17,18 @@ public class LoadingScene : MonoBehaviour
     public Slider slider;
     private float adjustLoadingTime = 1000000000.0f;
 
-    private readonly string authentication_url = "http://20.15.114.131:8080/api/login";
-    private readonly string api_key = "NjVjNjA0MGY0Njc3MGQ1YzY2MTcyMmNiOjY1YzYwNDBmNDY3NzBkNWM2NjE3MjJjMQ";
-
-    /* public void Awake() 
+    public async Task LoadScene(int sceneID) 
     {
-        Application.targetFrameRate = 60;
-    } */
-
-    public void LoadScene(int sceneID) 
-    {
-        StartCoroutine(LoadSceneAsync(sceneID));
+        await LoadSceneAsync(sceneID);
     }
 
-    IEnumerator LoadSceneAsync(int sceneID)
+    public async Task LoadSceneAsync(int sceneID)
     {
-        
-        if (sceneID == 1) 
+        if (sceneID == 1)
         {
-            // Call the JavaScript function defined in req_manager.js
-            UnityEngine.Coroutine token = StartCoroutine(CallAuthenticate());
+            bool tokenReceived = await CallAuthenticate();
 
-            if (token != null)
+            if (tokenReceived)
             {
                 AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
                 LoadingScreen.SetActive(true);
@@ -43,64 +37,66 @@ public class LoadingScene : MonoBehaviour
                 {
                     float progressValue = Mathf.Clamp01(operation.progress / adjustLoadingTime);
                     slider.value = progressValue;
+                    await Task.Yield(); // Allow other tasks to execute
                 }
             }
             else
             {
                 Debug.LogError("Authentication failed.");
             }
-            
-            yield return null;
         }
-
-
         else
         {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
-        LoadingScreen.SetActive(true);
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+            LoadingScreen.SetActive(true);
 
-        while (!operation.isDone)
-        {
-            float progressValue = Mathf.Clamp01(operation.progress / adjustLoadingTime);
-            slider.value = progressValue;
-            yield return null;
+            while (!operation.isDone)
+            {
+                float progressValue = Mathf.Clamp01(operation.progress / adjustLoadingTime);
+                slider.value = progressValue;
+                await Task.Yield(); // Allow other tasks to execute
+            }
         }
-        } 
-
-
-        /* AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
-        LoadingScreen.SetActive(true);
-
-        while (!operation.isDone)
-        {
-            float progressValue = Mathf.Clamp01(operation.progress / adjustLoadingTime);
-            slider.value = progressValue;
-            yield return null;
-        } */
 
     }
 
-    IEnumerator CallAuthenticate()
+    public async Task<bool> CallAuthenticate()
     {
-        string jsonData = "{\"apiKey\": \"" + this.api_key + "\"}";
+        string api_key = "NjVjNjA0MGY0Njc3MGQ1YzY2MTcyMmNiOjY1YzYwNDBmNDY3NzBkNWM2NjE3MjJjMQ";
 
-        UnityWebRequest www = new UnityWebRequest(this.authentication_url, "POST");
-
-        www.SetRequestHeader("Content-Type", "application/json");
-
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
-        www.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        www.downloadHandler = new DownloadHandlerBuffer();
-
-        if (www.result != UnityWebRequest.Result.Success)
+        try
         {
-            Debug.LogError(www.error);
-        }
-        else
-        {
-            Debug.Log("Response: " + www.downloadHandler.text);
-        }
+            using (var client = new HttpClient())
+            {
+                var jsonData = JsonConvert.SerializeObject(new { apiKey = this.apiKey });
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
 
-        yield return  www.SendWebRequest();
+                var response = await client.PostAsync("http://20.15.114.131:8080/api/login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<AuthenticationResponse>(responseContent);
+                    this.jwtToken = data.Token;
+                    return true;
+                }
+                else
+                {
+                    var errorMessage = await response.Content.ReadAsStringAsync();
+                    throw new Exception(errorMessage);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Authentication failed: {ex.Message}");
+            return false;
+        }
+    
+    }
+
+    private class AuthenticationResponse
+    {
+        public string Token { get; set; }
     }
 }
