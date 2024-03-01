03b88a4f4663e817f82d5e32994508bec1af1d0f
Thisari Piyumika Amarasekara <129175106+thisariii01@users.noreply.github.com>
2024-03-01 19:29:46 +0530

    Add files via upload

---
+++ Phase 0 Player Authentication/Request Manager/req_manager.js
@@ -0,0 +1,64 @@
+class HTTPRequestManager {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.jwtToken = null;
+  }
+
+  async authenticate() {
+    try {
+      const response = await fetch('http://20.15.114.131:8080/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ apiKey: this.apiKey })
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        this.jwtToken = data.token;
+        return true;
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      console.error('Authentication failed:', error);
+      return false;
+    }
+  }
+
+  async makeRequest(url, method = 'GET', body = null) {
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    if (this.jwtToken) {
+      headers['Authorization'] = `Bearer ${this.jwtToken}`;
+    }
+
+    const requestOptions = {
+      method: method,
+      headers: headers,
+    };
+
+    if (body) {
+      requestOptions.body = JSON.stringify(body);
+    }
+
+    try {
+      const response = await fetch(url, requestOptions);
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+    } catch (error) {
+      console.error('Request failed:', error);
+      return null;
+    }
+  }
+}
+
+export default HTTPRequestManager;
---
+++ Phase 0 Player Authentication/Request Manager/test.js
@@ -0,0 +1,18 @@
+import HTTPRequestManager from './req_manager.js';
+
+const apiKey = 'NjVjNjA0MGY0Njc3MGQ1YzY2MTcyMmNiOjY1YzYwNDBmNDY3NzBkNWM2NjE3MjJjMQ';
+const requestManager = new HTTPRequestManager(apiKey);
+
+async function initializeGame() {
+  const isAuthenticated = await requestManager.authenticate();
+  if (isAuthenticated) {
+    console.log('Successfully authenticated');
+    let data = await requestManager.makeRequest('http://20.15.114.131:8080/api/user/profile/view', 'GET'); 
+    console.log(data);
+  } 
+  else {
+    console.log('Failed to authenticate');
+  }
+}
+
+initializeGame();
