d10c08bd236a9df504a2c573cca47081cbfabf71
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-03-01 10:36:19 +0530

    main menu scene

---
+++ unity_project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame(){
+        Debug.Log("Play game");
+    }
+
+    public void PlayerProfile(){
+        Debug.Log("Player profile");
+    }
+
+    public void Leaderboard(){
+        Debug.Log("Leaderboard");
+    }
+
+    public void ExitGame(){
+        Debug.Log("Exit Game");
+    }
+}
