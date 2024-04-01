60446580617d7ab14032e77322a05307b99c2fdd
Sandeepa Dilshan <dilshansandeepa722@gmail.com>
2024-04-01 20:21:21 +0530

    Update MainMenuController.cs

--- unity_project/Assets/Scripts/MainMenuController.cs
+++ unity_project/Assets/Scripts/MainMenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class MainMenuController : MonoBehaviour
 
     public void PlayerProfile(){
         Debug.Log("Player profile");
+        SceneManager.LoadScene(2);
 /* 
         requestManager = ScriptableObject.CreateInstance<RequestManager>();
 
