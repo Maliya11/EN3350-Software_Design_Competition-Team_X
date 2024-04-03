d925e2bbf67b7ba8ccac519778b707cc32d35d01
KumuduK <129822318+KumuduK@users.noreply.github.com>
2024-04-03 09:24:04 +0530

    frontend

--- Quesstionaire/Frontend/quiz/src/components/quiz/Quiz.jsx
+++ Quesstionaire/Frontend/quiz/src/components/quiz/Quiz.jsx
@@ -8,6 +8,9 @@ const Quiz = () => {
     let [lock, setLock] = useState(false);
     let [score, setScore] = useState(0);
     let [result, setResult] = useState(false);
+    let [submitDisabled, setSubmitDisabled] = useState(true);
+    let [feedback, setFeedback] = useState(false);
+    let [answer, setAnswer] = useState(0);
 
     let Option1 = useRef(null);
     let Option2 = useRef(null);
@@ -15,17 +18,19 @@ const Quiz = () => {
     let Option4 = useRef(null);
 
     let option_array = [Option1,Option2,Option3,Option4];
+    let feed_array =[question.feed1,question.feed2,question.feed3,question.feed4];
 
     const checkAns = (e,ans) => {
         if (lock===false){
+            setAnswer(ans);
             if (question.ans===ans){
                 e.target.classList.add("correct");
-                setLock(true);
-                setScore(prev=>prev+1)
+                setSubmitDisabled(false);
+                setScore(prev=>prev+1);
             }
             else{
                 e.target.classList.add("wrong");
-                setLock(true);
+                setSubmitDisabled(false);
                 option_array[question.ans-1].current.classList.add("correct");
             }
         }
@@ -41,6 +46,8 @@ const Quiz = () => {
             setIndex(++index);
             setQuestion(data[index]);
             setLock(false);
+            setFeedback(false);
+            setAnswer(0);
             option_array.map((option)=>{
                 option.current.classList.remove("wrong");
                 option.current.classList.remove("correct");
@@ -55,14 +62,28 @@ const Quiz = () => {
         setScore(0);
         setLock(false);
         setResult(false);
+        setFeedback(false);
+        setAnswer(0);
+    }
+
+    const submit = ()=>{
+        if (submitDisabled==false){
+            // const correctOptionIndex = question.ans - 1;
+            // option_array[correctOptionIndex].current.classList.add("correct");
+            setSubmitDisabled(true);
+            setLock(true);
+            setFeedback(true);
+        }
+
     }
 
 
   return (
     <div className='conntainer'>
-        <h1>Questionnaire <button className="top-right-button" onClick={next}>Next</button></h1>
-        <hr />
+        <h1>Questionnaire </h1>
         {result?<></>:<>
+        <button className="top-right-button" onClick={next}>Next</button>
+        <hr />
         <h2>{index+1}.{question.question}</h2>
         <ul>
             <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
@@ -70,7 +91,13 @@ const Quiz = () => {
             <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
             <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
-        {/* <button onClick={next}>Next</button> */}
+        <button onClick={submit}>Submit</button>
+        {feedback?<>
+        <div>
+            <p>{question.feedbackg}</p>
+            <p>{feed_array[answer-1]}</p>
+        </div>
+        </>:<></>}
         <div className='index'>{index+1} of 10 questions</div>
         </>}
         {result?<>
