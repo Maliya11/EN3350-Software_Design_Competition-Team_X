71243044c51b720953430337a56314551f79286d
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-04-15 19:12:03 +0530

    a

--- Quesstionaire/Backend/src/main/java/com/project/backend/controller/PlayerController.java
+++ Quesstionaire/Backend/src/main/java/com/project/backend/controller/PlayerController.java
@@ -18,10 +18,17 @@ public class PlayerController {
 
     @PostMapping("/authenticate")
     public Map<String,Object> playerStateIdentify(@RequestBody Map<String, String> requestBody){
+        //set all player states to zero(as not playing)
         playerService.setPlayerStatesToZero();
+
+        //get the apiKey seperately as a string
         String apiKey = requestBody.get("apiKey");
+
+        //Identify or register player and set player state as active(playing)
         boolean validKey = playerService.playerStateIdentify(apiKey);
         Player activePlayer = playerService.identifyActivePlayer();
+
+        //Send to unity whether the apiKey is valid and number of questions that the player have completed
         Integer completedQuestions = activePlayer.getCompletedQuestions();
         Map<String, Object> response = new HashMap<>();
         response.put("validKey", validKey);
@@ -31,14 +38,20 @@ public class PlayerController {
 
     @GetMapping("/details")
     public Player sendPlayerDetails(){
+        //getting the details of the current player
         return playerService.identifyActivePlayer();
     }
 
 
     @PostMapping("/answer")
     public void playerAnswerSubmit(@RequestBody AnswerUpdateRequest request){
+        //identify the active player
         Player player = playerService.identifyActivePlayer();
+
+        //save the submitted answer in the player table
         playerService.playerAnswerSubmit(request.getqNum(), request.getSelAns());
+
+        //increment the completed questions of the player by one
         playerService.incrementCompletedQuestion(player);
     }
 }
