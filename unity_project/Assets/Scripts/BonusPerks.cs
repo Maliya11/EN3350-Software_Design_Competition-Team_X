fd9a936703ae6f688c1e89d75fe643b0b09ee7fc
Thisari Piyumika Amarasekara <tpamarasekara@gmail.com>
2024-04-17 00:00:23 +0530

    Updated DemoGameScene

---
+++ unity_project/Assets/Scripts/BonusPerks.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BonusPerks : MonoBehaviour
+{
+    // Get bonus perks for the player
+    private int bonusPerks;
+
+    // UI elements
+    public TextMeshProUGUI bonusPerksPanelText;
+
+    private void Start()
+    {
+        bonusPerks = PlayerPrefs.GetInt("questionnaireBonus");
+        bonusPerksPanelText.text = "Questionnaire Perks: " + bonusPerks;
+        bonusPerksPanelText.fontStyle = FontStyles.Bold;
+    }
+}
