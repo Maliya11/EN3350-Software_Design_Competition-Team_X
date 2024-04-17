3fddb2867eec31d1aa514c977a862cec42e4841b
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-04-17 18:17:01 +0530

    a

--- Quesstionaire/Backend/src/main/java/com/project/backend/service/QuestionService.java
+++ Quesstionaire/Backend/src/main/java/com/project/backend/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.project.backend.service;
 import com.project.backend.entity.Question;
 import com.project.backend.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +16,12 @@ public class QuestionService {
     QuestionDao questionDao;
 
     // Method to retrieve all questions from the database
-    public List<Question> getAllQuestions() {
+    public List<Question> getAllQuestionsSortedById() {
         try {
+            // Define sorting based on the id column
+            Sort sort = Sort.by(Sort.Direction.ASC, "id");
             // Retrieve all questions from the database
-            return questionDao.findAll();
+            return questionDao.findAll(sort);
         }catch (Exception e){
             // Handle exceptions
             throw new RuntimeException("An error occurred while retrieving questions.", e);
