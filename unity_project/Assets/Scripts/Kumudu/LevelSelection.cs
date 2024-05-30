371bf872fb6761f0d4198745a987e5332be92267
KumuduK <129822318+KumuduK@users.noreply.github.com>
2024-05-31 02:46:27 +0530

    Updated with character selection map selection and level selection

---
+++ unity_project/Assets/Scripts/Kumudu/LevelSelection.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public bool isUnlocked = false;
+    public Image lockImage;//LOCK IMAGE
+    public Image[] starsImages;//THREE STAR IMAGE
+
+    public Sprite[] starsSprites;
+
+    private void Update()
+    {
+        UpdateLevelButton();//TODO Remove later
+        UnlockLevel();
+    }
+
+    private void UnlockLevel()
+    {
+        int previousLvIndex = int.Parse(gameObject.name) - 1;// PlayerPrefs.GetInt("Lv" + gameObject.name) - 1;
+        if(PlayerPrefs.GetInt("Lv" + previousLvIndex) > 0)//At least get one stars in previous level
+        {
+            isUnlocked = true;//can unlock the next level
+        }
+    }
+
+    private void UpdateLevelButton()
+    {
+        if(isUnlocked)//MARKER We can play this level
+        {
+            lockImage.gameObject.SetActive(false);//we dont want to see the lock image
+            for(int i = 0; i < starsImages.Length; i++)
+            {
+                starsImages[i].gameObject.SetActive(true);
+            }
+
+            for(int i = 0; i < PlayerPrefs.GetInt("Lv" + gameObject.name); i++)
+            {
+                starsImages[i].sprite = starsSprites[i];
+            }
+        }
+        else
+        {
+            lockImage.gameObject.SetActive(true);
+            for (int i = 0; i < starsImages.Length; i++)
+            {
+                starsImages[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    //MARKER We have remvoed this method from UIMANAGER for easy to read and operator
+    public void SceneTransition(string _sceneName)
+    {
+        if(isUnlocked)
+        {
+            UIManager.instance.HideAllUIPanels();
+            SceneManager.LoadScene(_sceneName);    
+        }
+    }
+}
---
+++ unity_project/Assets/Scripts/Kumudu/UIManager.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject mapSelectionPanel;
+    public GameObject characterSelectionPanel;
+    public GameObject[] levelSelectionPanels;
+
+    [Header("Our STAR UI")]
+    public int stars;
+    public TextMeshProUGUI startText;
+    public MapSelection[] mapSelections;
+    public TextMeshProUGUI[] questStarsTexts;
+    public TextMeshProUGUI[] lockedStarsTexts;
+    public TextMeshProUGUI[] unlockStarsTexts;
+
+    
+    //new edit
+    // public CharacterSelect characterSelectionPanel;
+  
+    
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this; 
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    private void Update()//TODO REmove this method because we don't want to call these methods each frame
+    {
+        UpdateStarUI();
+        UpdateLockedStarUI();
+        UpdateUnLockedStarUI();
+    }
+
+    private void UpdateLockedStarUI()
+    {
+        for(int i = 0; i < mapSelections.Length; i++)
+        {
+            questStarsTexts[i].text = mapSelections[i].questNum.ToString();
+
+            if (mapSelections[i].isUnlock == false)//If one of the Map is locked
+            {
+                lockedStarsTexts[i].text = stars.ToString() + "/" + mapSelections[i].endLevel * 3;
+            }
+        }
+    }
+
+    private void UpdateUnLockedStarUI()//TODO FIXED LATER this method
+    {
+        for(int i = 0; i < mapSelections.Length; i++)
+        {
+            unlockStarsTexts[i].text = stars.ToString() + "/" + mapSelections[i].endLevel * 3;
+
+            switch(i)
+            {
+                case 0://MARKER MAP 01
+                    unlockStarsTexts[i].text = (PlayerPrefs.GetInt("Lv" + 1) + PlayerPrefs.GetInt("Lv" + 2) + PlayerPrefs.GetInt("Lv" + 3)) + "/" + (mapSelections[i].endLevel - mapSelections[i].startLevel + 1) * 3;
+                    break;
+                case 1://MARKER MAP 02
+                    unlockStarsTexts[i].text = (PlayerPrefs.GetInt("Lv" + 4) + PlayerPrefs.GetInt("Lv" + 5) + PlayerPrefs.GetInt("Lv" + 6)) + "/" + (mapSelections[i].endLevel - mapSelections[i].startLevel + 1) * 3;
+                    break;
+            }
+        }
+    }
+
+    //MARKER Update OUR Stars UI on the top left connor
+    private void UpdateStarUI()
+    {
+        stars = PlayerPrefs.GetInt("Lv" + 1) + PlayerPrefs.GetInt("Lv" + 2) + PlayerPrefs.GetInt("Lv" + 3) + PlayerPrefs.GetInt("Lv" + 4)
+         + PlayerPrefs.GetInt("Lv" + 5) + PlayerPrefs.GetInt("Lv" + 6);
+        startText.text = stars.ToString();
+    }
+
+    public void PressMapButton(int _mapIndex)//MARKER This method will be triggered when we press the (FOUR) map button
+    {
+        if(mapSelections[_mapIndex].isUnlock == true)//You can open this map
+        {
+            levelSelectionPanels[_mapIndex].gameObject.SetActive(true);
+            mapSelectionPanel.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("You cannot open this map now. Please work hard to collect more stars");
+        }
+    }
+
+    public void BackButton()
+    {
+        mapSelectionPanel.gameObject.SetActive(true);
+        for(int i = 0; i < mapSelections.Length; i++)
+        {
+            levelSelectionPanels[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void HideAllUIPanels()
+    {
+        mapSelectionPanel.gameObject.SetActive(false);
+        for (int i = 0; i < levelSelectionPanels.Length; i++)
+        {
+            levelSelectionPanels[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void BackMapSelection()//MARKER this method will be call on the SingleLevel button event
+    {
+
+        mapSelectionPanel.gameObject.SetActive(true);
+        for (int i = 0; i < mapSelections.Length; i++)
+        {
+            levelSelectionPanels[i].gameObject.SetActive(false);
+        }
+
+        SceneManager.LoadScene("Kumudu");
+    }
+}
