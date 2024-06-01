82de9d1168cb8b6ae96b97ed775b9f269021df24
Thisari Piyumika Amarasekara <tpamarasekara@gmail.com>
2024-06-01 20:55:03 +0530

    Modified energy scripts

--- unity_project/Assets/Scripts/Thisari/EnergyDataFetch.cs
+++ unity_project/Assets/Scripts/Thisari/EnergyDataFetch.cs
@@ -14,22 +14,11 @@ public class EnergyDataFetch : MonoBehaviour
     // URL to view yearly power consumption
     private string yearlyPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/yearly/view";
 
-    /* // URL to view power consumption by specific month
-    private string monthlyPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/month/view";
-
-    // URL to view power consumption by current month
-    private string currentMonthPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/current-month/view"; */
-
     // URL to view daily power consumption by specific month
     private string specificMonthDailyPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/month/daily/view";
-    private JSONNode specificMonthDailyPowerConsumption;
 
     // URL to view daily power consumption by current month
     private string currentMonthDailyPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/current-month/daily/view";
-    private JSONNode currentMonthDailyPowerConsumption;
-
-    /* // URL to view all power consumption
-    private string allPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/all/view"; */
 
     // URL to view current power consumption
     private string currentPowerConsumptionURL = "http://20.15.114.131:8080/api/power-consumption/current/view";
