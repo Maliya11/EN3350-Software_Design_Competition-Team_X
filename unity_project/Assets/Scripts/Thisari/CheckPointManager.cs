aa28a7153bde6e7c3d33fc9ca343c566af5dd881
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-01 17:21:20 +0530

    starting position same in all levels updated

--- unity_project/Assets/Scripts/Thisari/CheckPointManager.cs
+++ unity_project/Assets/Scripts/Thisari/CheckPointManager.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class CheckPointManager : MonoBehaviour
 {
-    PlayerManager playerManager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             // Update playerSafePosition of Player Manager to the current position of the checkpoint
-            playerManager.playerSafePosition = transform.position;
-            Debug.Log("Player safe position updated to: " + playerManager.playerSafePosition);        
+            PlayerManager.playerSafePosition = transform.position;
+            Debug.Log("Player safe position updated to: " + PlayerManager.playerSafePosition);        
         }
     }
 }
